f893a95c7837aae79fe2688ab5076de4190792d8
Bridget <bridgetoconnor808@gmail.com>
2021-02-04 19:36:01 -1000

    npm test is running now

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,13 +1,13 @@
 import assert from "assert"
-import parse from "../src/parser"
+import parse from "../src/parser.js"
 
 const goodPrograms = [
-  `mamaSays "make a muffin" ;)
+  String.raw`mamaSays "make a muffin" ;)
 --[=] Include more sugar
 
 yay!
  [=]--`,
-  `recipe salty pudding (salty chocolate, spicy water) (^-^)~
+  String.raw`recipe salty pudding (salty chocolate, spicy water) (^-^)~
 
  recipe salty cream (salty milk) (^-^)~
      serve milk + "happiness" ;)
@@ -19,40 +19,40 @@ yay!
    serve "no pudding today" ;)
  ~(^-^)
 ~(^-^)`,
-  "mamaSays 1+2*3 ;)",
-  `"oh no" ;)`,
-  `mamaSays (  	123   ) ;)`,
-  `mamaSays (2+4)*7 ;)`,
-  `mamaSays 2 * 3 ;)`,
-  `ingredient bitter g = 5 * 10 ;)`,
-  `(@) 3, 4, 5 (@) ;)`,
-  `8 * 9 + (9 - 5) ;)`,
-  `bake ingredient bitter egg = 1 until egg < 40 egg++ (^-^)~ 
+String.raw`mamaSays 1+2*3 ;)`,
+String.raw`"oh no" ;)`,
+String.raw`mamaSays (  	123   ) ;)`,
+String.raw`mamaSays (2+4)*7 ;)`,
+String.raw`mamaSays 2 * 3 ;)`,
+String.raw`ingredient bitter g = 5 * 10 ;)`,
+String.raw`(@) 3, 4, 5 (@) ;)`,
+String.raw`8 * 9 + (9 - 5) ;)`,
+String.raw`bake ingredient bitter egg = 1 until egg < 40 egg++ (^-^)~ 
 ~(^-^)`,
   `serve a + b ;)`,
   `recipe bitter Add (bitter a, bitter b) (^-^)~
   serve a + b ;)
 ~(^-^)`,
-  `ingredient spicy(@) rawEggs = (@) raw, raw (@) ;)`,
-  `ingredient spicy(@)(@) boolArrArr = (@) (@)raw(@), (@)raw, cooked(@) (@) ;)`,
-  `ingredient bitter[#][#] dictDict= 
+String.raw`ingredient spicy(@) rawEggs = (@) raw, raw (@) ;)`,
+String.raw`ingredient spicy(@)(@) boolArrArr = (@) (@)raw(@), (@)raw, cooked(@) (@) ;)`,
+String.raw`ingredient bitter[#][#] dictDict= 
 [#] 
     "key1" : [#] "inner1": 5[#] , 
     "key2" : [#] "inner2": 2[#]  
 [#];)`,
-  `ingredient salty[#] basicDict = [#]  "key" : "value" [#]  ;)`,
-  `serve -8 < 9 ;)`,
-  `serve -(-(-(-(-(-(-5)))))) ;)`,
-  `serve 4 || 9 ;)`,
-  `serve 3 && 5 || 2 ;)`,
-  `serve 3 || 5 && 2 * 5 + (5 - 1) ;)`,
-  `addAPinchOf grand (^-^)~
+String.raw`ingredient salty[#] basicDict = [#]  "key" : "value" [#]  ;)`,
+String.raw`serve -8 < 9 ;)`,
+String.raw`serve -(-(-(-(-(-(-5)))))) ;)`,
+String.raw`serve 4 || 9 ;)`,
+String.raw`serve 3 && 5 || 2 ;)`,
+String.raw`serve 3 || 5 && 2 * 5 + (5 - 1) ;)`,
+String.raw`addAPinchOf grand (^-^)~
 serve cooked ;)
 ~(^-^) orSubstitute another (^-^)~
 serve raw ;)
 ~(=^..^) saying something
 ~(^-^)`,
-  `recipe bland triangle () (^-^)~
+String.raw`recipe bland triangle () (^-^)~
 serve ;)
 --[=] 
 testing out 
@@ -60,18 +60,18 @@ multiple line
 comments
 [=]--
 ~(^-^)`,
-  `serve 2^2 ;)`,
+String.raw`serve 2^2 ;)`,
 ]
 
 const badPrograms = [
-  `ingredient bitter g = 5 * 10 ;);)`,
-  `ingredient spicy(@) = (@) raw, raw (@) ;)`,
-  `ingredient salty[#]  bad= [#] "key" [#] ;)`,
-  `serve -10 < 5 > -4 ;)`,
-  `serve -------5 ;)`,
-  `bake ingredient bitter egg = 1 u egg < 40 egg++ (^-^)~
+String.raw`ingredient bitter g = 5 * 10 ;);)`,
+String.raw`ingredient spicy(@) = (@) raw, raw (@) ;)`,
+String.raw`ingredient salty[#]  bad= [#] "key" [#] ;)`,
+String.raw`serve -10 < 5 > -4 ;)`,
+String.raw`serve -------5 ;)`,
+String.raw`bake ingredient bitter egg = 1 u egg < 40 egg++ (^-^)~
     ~(^-^)`,
-  `bake ingredient bitter egg = 1 until egg < 40 egg++ (^ -^)~
+String.raw`bake ingredient bitter egg = 1 until egg < 40 egg++ (^ -^)~
     ~(^-^)`,
 ]
 
