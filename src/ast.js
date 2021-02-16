2b7800dab76cd52c41f607006b4335ea5a69010e
Rachel Nguyen <rnguye12@lion.lmu.edu>
2021-02-15 19:19:35 -0600

    setting up ast

---
+++ src/ast.js
@@ -0,0 +1,103 @@
+import util from "util"
+
+/*
+- Use his AST printer/prettied function?
+- What needs to be a class, do we need to include literals and types, also do the names 
+need to correspond to the -- stuff in ohm? 
+- How does the ast builder work?
+- What do we do with the prettied function? / Do we need the prettied function?
+*/
+
+export class Program {
+  constructor(statements) {
+    this.statments = statements
+  }
+  [util.inspect.custom]() {
+    return prettied(this)
+  }
+}
+
+export class FunctionDeclaration {
+  constructor(name, parameters, body) {
+    Object.assign(this, { name, parameters, body })
+  }
+}
+
+export class VariableDeclaration {
+  constructor(variables, initializers) {
+    Object.assign(this, { variables, initializers })
+  }
+}
+
+export class Parameter {
+  constructor(name, type) {
+    Object.assign(this, { name, type })
+  }
+}
+
+export class Assignment {
+  constructor(targets, sources) {
+    Object.assign(this, { targets, sources })
+  }
+}
+
+export class ForLoop {
+  constructor(iterator, range, body) {
+    Object.assign(this, { iterator, range, body })
+  }
+}
+
+export class WhileLoop {
+  constructor(test, body) {
+    Object.assign(this, { test, body })
+  }
+}
+
+export class PrintStatement {
+  constructor(arguement) {
+    this.arguement = arguement
+  }
+}
+
+export class Break {}
+
+export class Return {
+  constructor(returnValue) {
+    this.returnValue = returnValue
+  }
+}
+
+function prettied(node) {
+  // Return a compact and pretty string representation of the node graph,
+  // taking care of cycles. Written here from scratch because the built-in
+  // inspect function, while nice, isn't nice enough.
+  const seen = new Map()
+  let nodeId = 0
+
+  function* prettiedSubtree(node, prefix, indent = 0) {
+    seen.set(node, ++nodeId)
+    let descriptor = `${" ".repeat(indent)}${prefix}: ${node.constructor.name}`
+    let [simpleProps, complexProps] = ["", []]
+    for (const [prop, child] of Object.entries(node)) {
+      if (seen.has(child)) {
+        simpleProps += ` ${prop}=$${seen.get(child)}`
+      } else if (Array.isArray(child) || (child && typeof child == "object")) {
+        complexProps.push([prop, child])
+      } else {
+        simpleProps += ` ${prop}=${util.inspect(child)}`
+      }
+    }
+    yield `${String(nodeId).padStart(4, " ")} | ${descriptor}${simpleProps}`
+    for (let [prop, child] of complexProps) {
+      if (Array.isArray(child)) {
+        for (let [index, node] of child.entries()) {
+          yield* prettiedSubtree(node, `${prop}[${index}]`, indent + 2)
+        }
+      } else {
+        yield* prettiedSubtree(child, prop, indent + 2)
+      }
+    }
+  }
+
+  return [...prettiedSubtree(node, "program")].join("\n")
+}
--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,10 @@
 import ohm from "ohm-js"
+import * as ast from "./ast.js"
 
-const grammar = ohm.grammar(String.raw`
+const bmkGrammar = ohm.grammar(String.raw`
 BigMamasKitchen {
   Program         =  Stmt*
-  Stmt            =  SimpleStmt terminate		                        -- simple
+  Stmt            =  SimpleStmt terminate		                          -- simple
                   |  Dec                                              -- dec            
                   |  addAPinchOf Exp Block        
                       (orSubstitute Exp Block)*
@@ -31,7 +32,7 @@ BigMamasKitchen {
                   |  ArrType
                   |  DictType
   ArrType         =  ArrType "(@)" 
-            |  Type "(@)"	
+                  |  Type "(@)"	
   DictType        =  DictType "[#]" 
                   |  Type "[#]" 
   Array           =  "(@)" ListOf<Exp, ","> "(@)"                     -- array
@@ -109,8 +110,63 @@ BigMamasKitchen {
   comment         =  "--[=]" (~"[=]--" any)* "[=]--"                    -- multiLine
                   | "~(=^..^)" (~"\n" any)*                              -- singleLine
 }`)
+/* eslint-disable no-unused-vars*/
+const astBuilder = bmkGrammar.createSemantics().addOperation("ast", {
+  Program(body) {
+    return new ast.Program(body.ast())
+  },
+  Statement_declare(_let, id, _eq, expression) {
+    return new ast.Declaration(id.sourceString, expression.ast())
+  },
+  Statement_assign(id, _eq, expression) {
+    return new ast.Assignment(
+      new ast.IdentifierExpression(id.sourceString),
+      expression.ast()
+    )
+  },
+  Statement_print(_print, expression) {
+    return new ast.PrintStatement(expression.ast())
+  },
+  Exp_binary(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Exp1_binary(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Term_binary(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
+  Term_unary(op, operand) {
+    return new ast.UnaryExpression(op.sourceString, operand.ast())
+  },
+  Factor_binary(left, op, right) {
+    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
+  },
 
-export default function parse(source) {
-  const match = grammar.match(source)
-  return match.succeeded()
+  Primary_parens(_open, expression, _close) {
+    return expression.ast()
+  },
+  Primary_unary(op, operand) {
+    return new ast.UnaryExpression(op.sourceString, operand.ast())
+  },
+  num(_base, _radix, _fraction) {
+    return new ast.LiteralExpression(+this.sourceString)
+  },
+  id(_firstChar, _restChars) {
+    return new ast.IdentifierExpression(this.sourceString)
+  },
+})
+/* eslint-enable no-unused-vars*/
+
+export default function parse(sourceCode) {
+  const match = bmkGrammar.match(sourceCode)
+  if (!match.succeeded()) {
+    throw new Error(match.message)
+  }
+  return astBuilder(match).ast()
 }
+
+// export default function parse(source) {
+//   const match = grammar.match(source)
+//   return match.succeeded()
+// }
